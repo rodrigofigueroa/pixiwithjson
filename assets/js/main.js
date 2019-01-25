d7d9c80e254ff80f01955e4b64d5ffc225d89b78
Rodrigo <olivertate274@gmail.com>
2019-01-25 16:02:55 -0600

    frames

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,5 +15,31 @@ let app = new application({
     resolution: 1       // default: 1
 });
 
-document.getElementById('canvas').appendChild(app.view);
+document.getElementById('canvasdiv').appendChild(app.view);
 
+
+loader
+.add('imagen','./assets/media/json/frames.json')
+.on('progress', progreso)
+.load(setup);
+
+function progreso(loader, progress){
+    console.log( loader.progress, progress.name);
+}
+
+function setup(){
+    
+    let princess, floor, clock;
+
+    let textureFloor = TextureCache['floor.png'];
+    floor = new sprite(
+        textureFloor
+    );
+    app.stage.addChild(floor);
+
+    let prin = new sprite(
+        resources['./assets/media/json/frames.json']
+    );
+
+
+}
