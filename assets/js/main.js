327a39d53006a760cb972f77c85272c5e2c33c66
Rodrigo <olivertate274@gmail.com>
2019-01-30 15:18:06 -0600

    agregando tesosroblob y caja

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,8 +8,8 @@ let application = PIXI.Application,
     Rectangle = PIXI.Rectangle;
 
 let app = new application({
-    width: 96,         // default: 800
-    height: 384,        // default: 600
+    width: 512,         // default: 800
+    height: 512,        // default: 600
     antialias: true,    // default: false
     transparent: true, // default: false
     resolution: 1       // default: 1
@@ -18,9 +18,33 @@ let app = new application({
 document.getElementById('canvasdiv').appendChild(app.view);
 
 loader
-.add('imagenname','../assets/media/json/frames.json')
+.add('imagenname','../assets/images/treasureHunter.json')
 .load(setup);
 
-function setup(){
+let princess, floor, clock;
 
+function setup(){
+   let tdungeon = TextureCache['dungeon.png'];
+   let dugeon = new sprite(tdungeon);
+   app.stage.addChild(dugeon);
+
+   let tdoor = TextureCache['door.png'];
+   let door = new sprite(tdoor);
+   app.stage.addChild(door);
+   door.x = 30;
+
+   let texplorer= TextureCache['explorer.png'];
+   let explorer = new sprite(texplorer);
+   app.stage.addChild(explorer);
+
+   explorer.x = 50;
+   explorer.y = app.stage.height / 2 - explorer.height / 2;
+
+   let ttreasure = TextureCache['treasure.png'];
+   let treasure = new sprite(ttreasure);
+   app.stage.addChild(treasure);
+
+   treasure.x = 400;
+   treasure.y = app.screen.height /2 - treasure.width/2;
+   
 }
